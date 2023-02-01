1a143b44afcf86259dfbf239a617034c39747d33
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-02-01 10:47:49 +0000

    Add direnv config

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 ln -nfs $PWD/config/.gitconfig $HOME/.gitconfig
 ln -nfs $PWD/config/.zshrc $HOME/.zshrc
+mkdir -p $HOME/.config/direnv/
+ln -nfs $PWD/config/direnv.toml $HOME/.config/direnv/direnv.toml
 $PWD/scripts/install-gh-cli.sh
 # TODO: Install preferred editor
 # TODO: Install Work script
