0ef0a56ca684606b16b4fa9043d859cb7a1adb49
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-10-24 16:55:20 +0100

    Add Brewfile

--- install.sh
+++ install.sh
@@ -3,10 +3,24 @@ ln -nfs $PWD/config/.gitconfig $HOME/.gitconfig
 ln -nfs $PWD/config/.zshrc $HOME/.zshrc
 mkdir -p $HOME/.config/direnv/
 ln -nfs $PWD/config/direnv.toml $HOME/.config/direnv/direnv.toml
-$PWD/scripts/install-gh-cli.sh
+# TODO: Don't need this if Homebrew exists
+# $PWD/scripts/install-gh-cli.sh
 $PWD/scripts/install-zsh-theme.sh
-# TODO: Install preferred editor
-# TODO: Install Work script
+brew bundle
 # TODO: Some sort of documentation of VS Code settings including snazzy.json
-# TODO: Install direnv?
-# TODO: Install Brewfile
+echo "Manually install the following"
+echo "- Chrome"
+echo "- Obsidian"
+echo "- Todoist"
+echo "- Bitwarden"
+echo "- Discord"
+echo "- Google Drive"
+echo "- WhatsApp"
+echo "- Microsoft Remote Desktop"
+echo "- Figma"
+echo "- Notion"
+echo "- Raindrop"
+echo "- AltTab"
+echo "- SteelSeries Exact Mouse Tool"
+echo "- Mionix Hub"
+echo "- Logitech G Hub"
